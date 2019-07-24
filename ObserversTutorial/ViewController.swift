cf0d8e636999405d768ea48e98f42867234b112d
geyoga <yogadewantama@gmail.com>
2019-07-24 21:14:59 +0700

    Initial Commit

---
+++ ObserversTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObserversTutorial
+//
+//  Created by Georgius Yoga Dewantama on 24/07/19.
+//  Copyright © 2019 Georgius Yoga Dewantama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
